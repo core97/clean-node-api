51fc55d5f2f15819b6fd6c26a9e88baab95f9a2b
Juan Cortés <jcortesg1997@gmail.com>
2021-10-20 00:12:21 +0200

    feat: wrap findOne function in MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient, ObjectId, Document } from 'mongodb'
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
@@ -9,5 +9,18 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  async findOne<T> (
+    collection: Collection<Document>,
+    { _id }: { _id: ObjectId | string }
+  ) {
+    return collection.findOne({ _id }) as unknown as
+      | (Omit<T, 'id'> & { _id: string })
+      | undefined
   }
 }
